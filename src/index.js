487d816225f2a9dafabd2437e73d7424f1022ef1
Your Name <amasarfowaap@gmail.com>
2024-11-30 12:55:47 +0000

    added files in my index.html and index.js and added images in my image folder

---
+++ src/index.js
@@ -0,0 +1,78 @@
+let myName = ("Portia");
+console.log(myName);
+
+let age =(20 + "years of age");
+console.log(age);
+
+let x = 8
+let y = 3
+let result =(x + y);
+console.log(result);
+
+let A = 20
+let B = 5
+let results =(A - B);
+console.log(results);
+
+let C =10
+let D = 2
+let answer = (C / D);
+console.log(answer);
+
+let J =6
+let K=8
+let ans =(J * 8);
+console.log(ans);
+
+let reminder = 15 % 4;
+console.log(reminder);
+
+// Boolean
+let FootballGame = true;
+console.log(FootballGame);
+
+let myNumber =42
+console.log(typeof myNumber);
+
+let color ="what is your favorite color?";
+console.log(typeof color);
+
+
+
+
+
+
+
+
+
+// let myName = "Portia";
+// console.log("myName");
+
+// let myAge = "28";
+// console.log("myAge");
+
+// let x = "8";
+// console.log("x");
+// let y = "3";
+// console.log("y");
+// let results =(x + y);
+// console.log("results");
+
+// let A = "20";
+// console.log("A");
+// let B ="5";
+// console.log("B");
+// let result =(A - B);
+// console.log("result");
+
+// let C = "10";
+// console.log("C");
+// let D ="2";
+// console.log("D");
+// let cd = (C * D);
+// console.log("cd");
+
+
+
+
+
